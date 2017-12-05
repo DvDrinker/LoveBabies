608d582912a99f461df9d30682c9bb0084bc6193
balalaMQ <31610916+balalaMQ@users.noreply.github.com>
2017-12-05 14:55:22 +0800

    小康有毒
    加 post  请求方式

--- src/main/java/com/lanou/controller/UserController.java
+++ src/main/java/com/lanou/controller/UserController.java
@@ -6,8 +6,10 @@ import com.lanou.util.FastJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -21,17 +23,18 @@ public class UserController {
 
 
     //    根据ID查找用户信息
-    @RequestMapping(value = "/select.do")
+    @RequestMapping(value = "/select.do",method = RequestMethod.POST)
     public void selectUser(Integer userId, HttpServletResponse response) {
         User user = userService.selectUser(userId);
         FastJson.toJson(user, response);
     }
-    //    根据Id查找用户信息QWERTYUIOPASDFGHJKLZXCVBNM
-    @RequestMapping(value = "/login.do")
-    public void login(String userName ,String password, HttpServletResponse response) {
+    //登录
+    @RequestMapping(value = "/login.do",method = RequestMethod.POST)
+    public void login(String userName , String password, HttpServletResponse response, HttpSession httpSession) {
         List<User> users = userService.login(userName,password);
         boolean result= false;
         if (users.size()==1){
+            httpSession.setAttribute("users",users);
             FastJson.toJson(users, response);
 
         }
@@ -39,7 +42,7 @@ public class UserController {
 
     }
 
-    @RequestMapping(value = "/addUser.do")
+    @RequestMapping(value = "/addUser.do",method = RequestMethod.POST)
     public void addUser(String userName, String password, HttpServletResponse response) {
 
         boolean result = userService.addUser(userName, password);
@@ -47,21 +50,22 @@ public class UserController {
 
     }
     //用户名存在为:false 可用:true
-    @RequestMapping(value ="/selectUserName.do" )
+    @RequestMapping(value ="/selectUserName.do",method = RequestMethod.POST)
     public void selectUserName(String userName,HttpServletResponse response){
         boolean result = userService.selectUserName(userName);
         FastJson.toJson(result,response);
     }
     //成功true 失败false
-    @RequestMapping(value ="/updateUser.do")
+    @RequestMapping(value ="/updateUser.do",method = RequestMethod.POST)
     public void updateUser(User user,HttpServletResponse response){
         boolean result = userService.updateUser(user);
         FastJson.toJson(result,response);
     }
     //修改成功 true 失败(原密码不正确 false)
-    @RequestMapping(value ="/updatePassword.do")
+    @RequestMapping(value ="/updatePassword.do",method = RequestMethod.POST)
     public void  updatePassword(String password , String password1 ,Integer userId,HttpServletResponse response){
         boolean result = userService.updatePassword(userId,password,password1);
+
         FastJson.toJson(result,response);
     }
 }
